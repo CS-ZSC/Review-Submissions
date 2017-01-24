a3cb63299b6f7c4f811c5f33d95a326c56aefb3f
Mohamed A. Hamza <mohamed_anwar@opmbx.org>
2017-01-24 19:33:36 +0200

    Update fizzbuzz.py

--- Fizz Buzz - Python 2/fizzbuzz.py
+++ Fizz Buzz - Python 2/fizzbuzz.py
@@ -6,11 +6,11 @@ Created on Tue Jan 24 18:40:50 2017
 """
 
 for i in range(1,101):
-    if i%3==0:
-        print "fizz"
-    elif i%5==0:
+    if i % 3 == 0:  # Divisible by 3
+        print "Fizz"
+    elif i % 5 == 0:  # Divisible by 5
         print "Buzz"
-    elif i%5 and i%3 ==0:
-        print "fizzbuzz"
+    elif i % 5 and i % 3 == 0:   # Divisible by both 3 and 5
+        print "FizzBuzz"
     else:
         print i
