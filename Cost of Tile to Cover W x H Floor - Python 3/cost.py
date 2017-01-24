3447a92f1c2b636393ae841ff81245ea46808eda
Mohamed A. Hamza <mohamed_anwar@opmbx.org>
2017-01-24 20:09:07 +0200

    Create cost.py

---
+++ Cost of Tile to Cover W x H Floor - Python 3/cost.py
@@ -0,0 +1,6 @@
+print ("Tile Cost Calculator"+"\n"+"\n")
+W = int (input("Enter the width of the floor..."+"\n"))
+H = int (input("Enter the Height of the floor..."+"\n"))
+C = int (input("Enter the cost of the square meter of tile..."+"\n"))
+Total = W * H * C
+print ("The total cost is" + "\n" + str(Total))
