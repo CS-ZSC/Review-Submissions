4c8619987feb28d4666b318b67d724480ae32dd6
Mohamed A. Hamza <mohamed_anwar@opmbx.org>
2017-01-25 10:31:59 +0200

    Create count.py

---
+++ Count Vowels - Python 3/count.py
@@ -0,0 +1,16 @@
+A = input("Enter whatever you want: ")
+
+vowels = ['a', 'e', 'i', 'o', 'u']
+a = A.count('a')
+e = A.count('e')
+i = A.count('i')
+o = A.count('o')
+u = A.count('u')
+
+
+
+print("you've got", str(a), "a's in the string") 
+print("you've got", str(e), "e's in the string") 
+print("you've got", str(i), "i's in the string") 
+print("you've got", str(o), "o's in the string") 
+print("you've got", str(u), "u's in the string") 
